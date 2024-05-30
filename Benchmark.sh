5d7939939c457c906b9046db1c95cc8327315201
Dmytro Osipov <blackboneworks@gmail.com>
2024-05-30 16:30:13 +0200

    fix reports ordering

--- Benchmark.sh
+++ Benchmark.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 # pre-clean
 rm -rf ./.benchmark_results
 dotnet clean
@@ -22,10 +20,17 @@ dotnet run --project Benchmark/Benchmark.csproj -c Release --no-build /p:CheckCa
 
 rm report.md
 
-echo -e "HW Info:\n" >> report.md
+# Prepare the report file
+echo "# LOCAL RUN BENCHMARKS:\n---" >> report.md
+echo "HW Info:\n" >> report.md
 cat .benchmark_results/hwinfo >> report.md
-echo -e "\n" >> report.md
+echo "\n" >> report.md
+
+# Find and sort the markdown files by their first line
 find .benchmark_results -name '*.md' -print0 | while IFS= read -r -d '' file; do
+  first_line=$(head -n 1 "$file")
+  echo "$first_line|$file"
+done | sort | while IFS='|' read -r first_line file; do
   cat "$file" >> report.md
   echo -e "\n" >> report.md
 done
