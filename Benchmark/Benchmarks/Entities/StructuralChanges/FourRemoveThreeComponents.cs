0347e405fc3eb4bae9f0e49f41c6a51022fd941b
Dmytro Osipov <blackboneworks@gmail.com>
2024-07-27 19:38:23 +0200

    batch of structural changes benchmarks

---
+++ Benchmark/Benchmarks/Entities/StructuralChanges/FourRemoveThreeComponents.cs
@@ -0,0 +1,48 @@
+using System;
+using Benchmark._Context;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmark.Benchmarks.Entities.StructuralChanges;
+
+[ArtifactsPath(".benchmark_results/" + nameof(FourRemoveThreeComponents<T>))]
+[MemoryDiagnoser]
+[BenchmarkCategory(Categories.PerInvocationSetup)]
+#if CHECK_CACHE_MISSES
+[HardwareCounters(BenchmarkDotNet.Diagnosers.HardwareCounter.CacheMisses)]
+#endif
+public abstract class FourRemoveThreeComponents<T> : IBenchmark<T> where T : IBenchmarkContext
+{
+    [Params(Constants.EntityCount)] public int EntityCount { get; set; }
+
+    public T Context { get; set; }
+    private Array _entitySet;
+
+    [IterationSetup]
+    public void Setup()
+    {
+        Context = BenchmarkContext.Create<T>(EntityCount);
+        Context?.Setup();
+        _entitySet = Context?.PrepareSet(EntityCount);
+        Context?.Warmup<Component1, Component2, Component3, Component4>(0);
+        Context?.CreateEntities(_entitySet, 0, default(Component1), default(Component2), default(Component3), default(Component4));
+        Context?.Warmup<Component2, Component3, Component4>(1);
+        Context?.FinishSetup();
+    }
+
+    [IterationCleanup]
+    public void Cleanup()
+    {
+        Context?.DeleteEntities(_entitySet);
+        Context?.Cleanup();
+        Context?.Dispose();
+        Context = default;
+    }
+
+    [Benchmark]
+    public void Run()
+    {
+        Context?.Lock();
+        Context?.RemoveComponent<Component2, Component3, Component4>(_entitySet, 1);
+        Context?.Commit();
+    }
+}
---
+++ Benchmark/Benchmarks/Entities/StructuralChanges/TwoAddOneComponent.cs
@@ -0,0 +1,48 @@
+using System;
+using Benchmark._Context;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmark.Benchmarks.Entities.StructuralChanges;
+
+[ArtifactsPath(".benchmark_results/" + nameof(TwoAddOneComponent<T>))]
+[MemoryDiagnoser]
+[BenchmarkCategory(Categories.PerInvocationSetup)]
+#if CHECK_CACHE_MISSES
+[HardwareCounters(BenchmarkDotNet.Diagnosers.HardwareCounter.CacheMisses)]
+#endif
+public abstract class TwoAddOneComponent<T> : IBenchmark<T> where T : IBenchmarkContext
+{
+    [Params(Constants.EntityCount)] public int EntityCount { get; set; }
+
+    public T Context { get; set; }
+    private Array _entitySet;
+
+    [IterationSetup]
+    public void Setup()
+    {
+        Context = BenchmarkContext.Create<T>(EntityCount);
+        Context?.Setup();
+        _entitySet = Context?.PrepareSet(EntityCount);
+        Context?.Warmup<Component1, Component2>(0);
+        Context?.CreateEntities(_entitySet, 0, default(Component1), default(Component2));
+        Context?.Warmup<Component3>(1);
+        Context?.FinishSetup();
+    }
+
+    [IterationCleanup]
+    public void Cleanup()
+    {
+        Context?.DeleteEntities(_entitySet);
+        Context?.Cleanup();
+        Context?.Dispose();
+        Context = default;
+    }
+
+    [Benchmark]
+    public void Run()
+    {
+        Context?.Lock();
+        Context?.AddComponent<Component3>(_entitySet, 1);
+        Context?.Commit();
+    }
+}
